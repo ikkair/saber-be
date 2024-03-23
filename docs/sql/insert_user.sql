39c20eaa3189a6fed1e4c63dab4b85802a903ef1
ikkair <ikhlasul.kamal.i@gmail.com>
2024-03-23 18:55:45 +0700

    Adding mock users

---
+++ docs/sql/insert_user.sql
@@ -0,0 +1,13 @@
+-- Admin
+INSERT INTO users (id, name, email, password, role)
+VALUES ('1', 'Admin', 'admin@mail.com', '$2a$10$0KNW/Y/Pg258u4BtvC01Ueka01/i.bnIqBt6ZufVar2IQu0H2hSb.', 'admin');
+
+-- Users
+INSERT INTO users (id, name, email, password, role)
+VALUES ('2', 'Budi', 'budi@mail.com', '$2a$10$CDxOlDOo5A4VHYcANWqwUOPgr/pDIWaOK/mC67DKIQ5MyzJd8.oF.', 'user');
+INSERT INTO users (id, name, email, password, role)
+VALUES ('3', 'Sintia', 'sintia@mail.com', '$2a$10$VGOWNO838OHqSTVrefJ.g.vjbwkeYkSU/ZV5b1Sm74KD6n05pCVkO', 'user');
+INSERT INTO users (id, name, email, password, role)
+VALUES ('4', 'Rudi', 'rudi@mail.com', '$2a$10$tYyouRIqAX1G/knjfEsH7OXIA/JlZWzk1m2QOh3uTZ0Mn.kQK.tgm', 'user');
+INSERT INTO users (id, name, email, password, role)
+VALUES ('5', 'Hana', 'hana@mail.com', '$2a$10$F00dnrSxyHTwl6C9eTl.mODK1wUr8brq/U9xxL9xagG0xmSdPzG02', 'user');
